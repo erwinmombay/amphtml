fefcebf7830cf3c670f7fa57a0f415305f905280
Erwin Mombay <erwinm@google.com>
2018-06-25 15:27:57 -0700

    update amp-geo-presets.js

--- extensions/amp-geo/0.1/amp-geo-presets.js
+++ extensions/amp-geo/0.1/amp-geo-presets.js
@@ -30,62 +30,61 @@
  */
 
 export const ampGeoPresets = {
-  /* Expanded EEA countries */
-  'preset-eea': [
-    /** BEGIN Machine Generated: EEA */
-    'AT', // Austria
-    'BE', // Belgium
-    'BG', // Bulgaria
-    'HR', // Croatia
-    'CY', // Cyprus
-    'CZ', // Czech Republic
-    'DK', // Denmark
-    'EE', // Estonia
-    'FI', // Finland
-    'FR', // France
-    'DE', // Germany
-    'GR', // Greece
-    'HU', // Hungary
-    'IS', // Iceland
-    'IE', // Ireland
-    'IT', // Italy
-    'LV', // Latvia
-    'LI', // Liechtenstein
-    'LT', // Lithuania
-    'LU', // Luxembourg
-    'MT', // Malta
-    'NL', // Netherlands
-    'NO', // Norway
-    'PL', // Poland
-    'PT', // Portugal
-    'RO', // Romania
-    'SK', // Slovakia
-    'SI', // Slovenia
-    'ES', // Spain
-    'SE', // Sweden
-    'GB', // United Kingdom
-    'AX', // Aland Islands
-    'IC', // Canary Islands
-    'EA', // Ceuta & Melilla
-    'GF', // French Guiana
-    'PF', // French Polynesia
-    'TF', // French Southern Territories
-    'GI', // Gibraltar
-    'GP', // Guadeloupe
-    'GG', // Guernsey
-    'JE', // Jersy
-    'MQ', // Martinique
-    'YT', // Mayotte
-    'NC', // New Caledonia
-    'RE', // Reunion
-    'BL', // St Barthelemy
-    'MF', // St Martin
-    'PM', // St Pierre & Miquelon
-    'SJ', // Svalbard & Jan Mayen Islands
-    'VA', // Vatican City
-    'WF', // Wallis & Futuna Islands
-    'EZ', // Eurozone
-    'CH', // Switzerland
+  'preset-gdpr': [
+    /** BEGIN Machine Generated: GDPR */
+    AT, // Austria
+    BE, // Belgium
+    BG, // Bulgaria
+    HR, // Croatia
+    CY, // Cyprus
+    CZ, // Czech Republic
+    DK, // Denmark
+    EE, // Estonia
+    FI, // Finland
+    FR, // France
+    DE, // Germany
+    GR, // Greece
+    HU, // Hungary
+    IS, // Iceland
+    IE, // Ireland
+    IT, // Italy
+    LV, // Latvia
+    LI, // Liechtenstein
+    LT, // Lithuania
+    LU, // Luxembourg
+    MT, // Malta
+    NL, // Netherlands
+    NO, // Norway
+    PL, // Poland
+    PT, // Portugal
+    RO, // Romania
+    SK, // Slovakia
+    SI, // Slovenia
+    ES, // Spain
+    SE, // Sweden
+    GB, // United Kingdom
+    AX, // Aland Islands
+    IC, // Canary Islands
+    EA, // Ceuta & Melilla
+    GF, // French Guiana
+    PF, // French Polynesia
+    TF, // French Southern Territories
+    GI, // Gibraltar
+    GP, // Guadeloupe
+    GG, // Guernsey
+    JE, // Jersy
+    MQ, // Martinique
+    YT, // Mayotte
+    NC, // New Caledonia
+    RE, // Reunion
+    BL, // St Barthelemy
+    MF, // St Martin
+    PM, // St Pierre & Miquelon
+    SJ, // Svalbard & Jan Mayen Islands
+    VA, // Vatican City
+    WF, // Wallis & Futuna Islands
+    EZ, // Eurozone
+    CH, // Switzerland
     /** END Machine Generated */
   ],
 };
