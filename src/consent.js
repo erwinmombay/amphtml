c0b60599224534ac5c07123b0db6459ec08e8367
erwin mombay <erwin.mombay@gmail.com>
2019-02-27 13:33:35 -0800

    Update consent.js

--- src/consent.js
+++ src/consent.js
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+// sdfs
 import {
   CONSENT_POLICY_STATE, // eslint-disable-line no-unused-vars
 } from './consent-state';
